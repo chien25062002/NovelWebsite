e5493e8de340d75da317026150c173df2c238179
shioiori <pna7702@gmail.com>
2023-04-04 15:33:27 +0700

    view component usernavbar

---
+++ NovelWebsite/NovelWebsite/Components/UserNavbarViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NovelWebsite.Entities;
+using System.Security.Claims;
+
+namespace NovelWebsite.Components
+{
+    public class UserNavbarViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _dbContext;
+
+        public UserNavbarViewComponent(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var claims = HttpContext.User.Identity as ClaimsIdentity;
+            var userId = _dbContext.Accounts.Where(a => a.AccountName == claims.FindFirst(ClaimTypes.NameIdentifier).Value).FirstOrDefault().UserId;
+            return View(userId);
+        }
+
+    }
+}
